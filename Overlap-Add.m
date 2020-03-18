1e986229484c69b918776b3288a631aca9a8cc0c
diu100kar <58329843+diu100kar@users.noreply.github.com>
2020-03-18 15:33:18 +0530

    Create Overlap-Add.m
    Function. x is the long sequence. h is the system's impulse response. L is the length of smaller chunks of the larger sequence.

---
+++ Overlap-Add.m
@@ -0,0 +1,18 @@
+function answ=shambharkar_2020_ovadd(x,h,L)
+    lx=length(x);
+    M=length(h);
+    N=M+L-1;
+    h=[h,zeros(1,L-1)];
+    H=fft(h,N);
+    x2=zeros(1,ceil(length(x)/L)*L);
+    x2(1:length(x))=x;
+    answ=zeros(1,length(x2)+M-1);
+    for i=1:L:length(x)
+        xtemp=[x2(1,i:i+L-1),zeros(1,M-1)];
+        tempfft=fft(xtemp,N);
+        tempY=tempfft.*H;
+        y=ifft(tempY,N);
+        answ(1,i:i+N-1)=answ(1,i:i+N-1)+y;
+    end
+    answ=round(answ(1,1:M+lx-1));
+end
